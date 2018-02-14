4a3754f09df5a719acd532113d57827e3d8a30e6
carlomarini <30346246+carlomarini@users.noreply.github.com>
2018-02-14 13:44:43 +0100

    Update creditscore.js

--- routes/creditscore.js
+++ routes/creditscore.js
@@ -11,7 +11,7 @@ exports.list = function(req, res){
 
 // _CHANGE_ : Please comment the line with the DUMMY message and uncomment the one with the Welcome message
         //"MESSAGE": "DUMMY message V1"
-        "MESSAGE": "JUVE - TOTTENAM 1-0"
+        "MESSAGE": "JUVE - TOTTENAM 2-0"
 
     };
 
